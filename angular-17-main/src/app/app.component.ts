1269df13f3e5d89a83c7735c997c80997d60af67
Rafael <raf.herrera@duocuc.cl>
2024-02-26 19:00:38 -0300

    version 0.1 -mejora de navbar

--- angular-17-main/src/app/app.component.ts
+++ angular-17-main/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Contenido1Component } from './contenido-1/contenido-1.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, NavbarComponent],
+  imports: [RouterOutlet, UserComponent, NavbarComponent, Contenido1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
